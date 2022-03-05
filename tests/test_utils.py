e5536fb3e40fd0533d577a48a7d25070eab1b98e
Jens Roland <jr@deepquantum.com>
2022-03-05 21:24:03 +0100

    Fixed unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,10 @@ from hooks.pre_gen_project import validate_line_length
 
 
 def test_validate_line_length():
-    assert validate_line_length(88) is None
+    try:
+        validate_line_length(88)
+    except ValueError as exc:
+        assert False, f"'validate_line_length' raised an exception {exc}"
 
     with pytest.raises(ValueError):
         validate_line_length(1_000)
